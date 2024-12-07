a28c7abd319ae7fc43a67a30c31027a4c97675b1
lopezajr <aldolopez0995@gmail.com>
2024-12-07 13:43:09 -0500

    Fixed unused imports in NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';  // Use NavLink for active links
-import { getFavorites } from '../utils/localStorage';
 import { IonIcon } from '@ionic/react';
 import { home, heart } from 'ionicons/icons';
 import '../styles/NavBar.scss';
 
 const NavBar = () => {
-
   return (
     <nav className="navbar">
       <div className="navbar-tabs">
